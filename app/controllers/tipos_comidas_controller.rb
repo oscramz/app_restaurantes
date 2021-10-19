303341db70eaed9aa7eea5ef5109b43326e26860
oscramz <oscram@gmail.com>
2021-10-18 22:11:18 -0300

    Incorporacion de logicas varias, conecciones a vistas y metodos

--- app/controllers/tipos_comidas_controller.rb
+++ app/controllers/tipos_comidas_controller.rb
@@ -3,5 +3,44 @@ class  TiposComidasController < ApplicationController
     # GET /tipos_comidas
     def listar
         @todos_los_tipos = TipoComida.all
+        @titulo_link = "Registrar nuevo tipo de comida"
     end
+
+    #get /tipos_comidas/nuevo
+    def crear
+        @nuevo_tipo_comida = TipoComida.new
+    end
+
+    #get /tipos_comidas/:id
+    def mostrar
+        #vista para mostrar detalle de tipo de comida
+        #por ejemplo, mostrar todos los rest adjuntos
+        @tipo_comida = TipoComida.find(params[:id])
+    end
+
+    #get / tipos_comidas/:id/editar
+    def editar
+        #mostrar formulario con los datos de un registro para editarlos
+    end
+
+    #post /tipos_comidas
+    def guardar
+        byebug
+        #guardar todo lo que llegue del formulario en la DB
+        datos_tipo_comida = params.require(:tipo_comida).permit(:tipo)
+        nuevo_tipo = TipoComida.new(datos_tipo_comida)
+        nuevo_tipo.save
+        redirect_to tipos_comidas_path
+    end
+
+    #DELETE tipos_comidas/:id
+    def eliminar
+        #pasos para eliminar un registro
+        #1. buscar el registro por ID
+        tipo = TipoCOmida.find(params[:id])
+        #2 Intentar eliminar el registro
+        tipo.destroy
+        redirect_to tipos_comidas_path
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  get 'tipos_comidas', to: 'tipos_comidas#listar'
+  # rutas tipos comidas
+  get    'tipos_comidas',            to: 'tipos_comidas#listar',   as: 'tipos_comidas'       #listar
+  get    'tipos_comidas/nuevo',      to: 'tipos_comidas#crear',    as: 'nuevo_tipo_comida'   #registro comida
+  get    'tipos_comidas/:id',        to: 'tipos_comidas#mostrar',  as: 'tipo_comida'         #detalle tipo de comida
+  get    'tipos_comidas/:id/editar', to: 'tipos_comidas#editar',   as: 'editar_tipo_comida'  #editar tipo comida
+
+  post   'tipos_comidas',            to: 'tipos_comidas#guardar'
+  delete 'tipos_comidas/:id',        to: 'tipos_comidas#eliminar'
 end
