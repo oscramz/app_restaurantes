e034d17b7eb576164ad46d8f36c7372047d1666e
oscramz <oscram@gmail.com>
2021-10-22 22:52:23 -0300

    agregar vista editar registro con form_with y parcial

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -4,14 +4,27 @@ class UsuariosController < ApplicationController
         @usuario = Usuario.new
     end
 
+    def mostrar
+        @usuario = Usuario.find(params[:id])
+    end
+
     def guardar
         datos_usuario = params.require(:usuario).permit(:nombre_usuario, :password, :password_confirmation)
         @usuario = Usuario.new(datos_usuario)
         if @usuario.save
             #mensaje de confirmacion
+            redirect_to usuario_path(@usuario)
         else
             render :crear
         end
+    end
+
+    def editar
+        @usuario = Usuario.find(params[:id])
+    end
+    
+    def eliminar
+
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,8 @@ Rails.application.routes.draw do
 
   #rutas usuarios
   get    'usuarios/nuevo',           to: 'usuarios#crear',        as: 'nuevo_usuario'
+  get    'usuarios/:id',             to: 'usuarios#mostrar',      as: 'usuario'
+  get    'usuarios/:id/editar',       to: 'usuarios#editar',       as: 'editar_usuario'
   
   post   'usuarios',                 to: 'usuarios#guardar',      as: 'usuarios'
 
