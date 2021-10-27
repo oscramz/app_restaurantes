7cce276c25f61e068d845eabc9cc7cdf8efa3199
oscramz <oscram@gmail.com>
2021-10-26 22:55:31 -0300

    limpieza de codigo en el usuarios_controller mediante metodos y filtros

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -1,19 +1,20 @@
 class UsuariosController < ApplicationController
+    before_action :listar_usuarios, only: [:mostrar, :editar, :actualizar, :eliminar]
 
     def crear
         @usuario = Usuario.new
     end
 
     def mostrar
-        @usuario = Usuario.find(params[:id])
+        
     end
 
     def editar
-        @usuario = Usuario.find(params[:id])
+        
     end
 
     def actualizar
-        @usuario = Usuario.find(params[:id])
+        
         params_usuario
         if @usuario.update(params_usuario)
             redirect_to usuario_path(@usuario)
@@ -35,7 +36,7 @@ class UsuariosController < ApplicationController
 
     
     def eliminar
-        @usuario = Usuario.find(params[:id])
+        
         if @usuario.destroy
             flash[:eliminar] = "Usuario #{@usuario.nombre_usuario} eliminado con éxito"
         else
@@ -47,6 +48,10 @@ class UsuariosController < ApplicationController
     private
     def params_usuario
         params.require(:usuario).permit(:nombre_usuario, :password, :password_confirmation)
+    end
+
+    def lista_usuarios
+        @usuario = Usuario.find(params[:id])
     end
 
 end
