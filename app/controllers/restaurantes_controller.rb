f0caa4d362911103f3919bf5e9dc4c6a18539a49
oscramz <oscram@gmail.com>
2021-10-30 22:55:50 -0300

    Correccion de errores y ajustes en el CRUD de restaurantes

--- app/controllers/restaurantes_controller.rb
+++ app/controllers/restaurantes_controller.rb
@@ -1,12 +1,16 @@
 class RestaurantesController < ApplicationController
 
+    before_action :asignar_restaurante, only: [:editar, :mostrar, :actualizar, :eliminar]
     def listar
         @todos_restaurantes = Restaurante.all
-        @tipos_comidas = TipoComida.all
     end
     
     def crear
         @restaurante = Restaurante.new
+        @tipos_comidas = TipoComida.all
+    end
+    
+    def editar
         @tipos_comidas = TipoComida.all
     end
 
@@ -14,13 +18,19 @@ class RestaurantesController < ApplicationController
         
     end
 
-    def editar
-        @restaurante = Restaurante.find(params[:id])
+    def actualizar
+        @restaurante.nombre = params_restaurantes[:nombre]
+        @restaurante.tipo_comida_id = params_restaurantes[:tipo_comida_id]
+        if @restaurante.save
+            redirect_to restaurantes_path
+        else
+            @tipos_comidas = TipoComida.all
+            render :editar
+        end
     end
 
     def guardar
-        datos_restaurantes = params.require(:restaurante).permit(:nombre, :tipo_comida_id)
-        @restaurante = Restaurante.new(datos_restaurantes)
+        @restaurante = Restaurante.new(params_restaurantes)
         if @restaurante.save
                 redirect_to restaurantes_path
         else
@@ -29,4 +39,18 @@ class RestaurantesController < ApplicationController
         end
     end
 
+    def eliminar
+        @restaurante.destroy
+        redirect_to restaurantes_path
+    end
+
+    private
+
+    def asignar_restaurante
+        @restaurante = Restaurante.find(params[:id])
+    end
+    
+    def params_restaurantes
+        datos_restaurantes = params.require(:restaurante).permit(:nombre, :tipo_comida_id)
+    end
 end
